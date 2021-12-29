444a3b01349e993fadf046259ab08f6f882886fe
SingleArtist <95147229+SingleArtist@users.noreply.github.com>
2021-12-29 19:33:59 +0300

    Add files via upload

---
+++ MPP1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+//Задание 1
+/*Создать класс на языке C#, который:
+- называется TaskQueue и реализует логику пула потоков;
+- создает указанное количество потоков пула в конструкторе;
+- содержит очередь задач в виде делегатов без параметров:
+delegate void TaskDelegate();
+- обеспечивает постановку в очередь и последующее выполнение
+делегатов с помощью метода
+void EnqueueTask(TaskDelegate task);*/
+
+namespace FirstTask
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            TaskQueue taskQueue = new TaskQueue(18);
+            for (int i = 0; i < 18; i++)
+            {
+                taskQueue.EnqueueTask(OurTask);
+            }
+ 
+            taskQueue.Finish();
+        }
+
+        private static void OurTask()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("current proc id = " + Thread.CurrentThread.ManagedThreadId + " task = " + i);
+            }
+        }
+    }
+}
---
+++ MPP1/TaskQueue.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+//Задание 1
+/*Создать класс на языке C#, который:
+- называется TaskQueue и реализует логику пула потоков;
+-создает указанное количество потоков пула в конструкторе;
+-содержит очередь задач в виде делегатов без параметров:
+delegate void TaskDelegate();
+-обеспечивает постановку в очередь и последующее выполнение
+делегатов с помощью метода
+void EnqueueTask(TaskDelegate task);*/
+
+namespace FirstTask
+{
+    public class TaskQueue
+    {
+        private Thread[] threadPoool;
+        public delegate void taskDelegate();
+        private Queue<taskDelegate> TaskDelegateQueue = new Queue<taskDelegate>();
+
+        public bool ContinueWork = true;
+
+        public TaskQueue(int quantityThread)
+        {
+            this.threadPoool = new Thread[quantityThread];
+            for (int i = 0; i < quantityThread; i++)
+            {
+                threadPoool[i] = new Thread(ThreadWork);//передается информация какой метод будет в потоке
+                threadPoool[i].Start();//запуск потока
+            }
+        }
+        public void ThreadWork()
+        {
+            taskDelegate task;
+            while (ContinueWork)
+            {
+                try
+                {
+                    while (TaskDelegateQueue.Count > 0)
+                    {
+                        task = null;
+                        lock (TaskDelegateQueue)//lock - значит не дает доступ к этому оду другим потокам
+                        {
+                            task = TaskDelegateQueue.Dequeue();//достаем задачу из очереди
+                        }
+                        if (task != null)
+                        {
+                            task.Invoke();//вызвать задачу (метод который находится в делегате)
+                        }
+                    }
+                }
+                catch
+                {//похороны
+                }
+            }
+        }
+        public void EnqueueTask(taskDelegate task)//заполнение очереди
+        {
+            lock (TaskDelegateQueue)
+            {
+                TaskDelegateQueue.Enqueue(task);//запись задачи в очередь
+            }
+        }
+
+
+
+        public void Finish()
+        {
+            ContinueWork = false;
+        }
+    }
+}
